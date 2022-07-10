0b7fb0faf18c88b49096ccb0d243f224c06ecbb0
Melbeck777 <molmin729@gmail.com>
2022-07-10 10:05:41 +0900

    価格データの取得範囲を取得する関数を追加

--- get_price.py
+++ get_price.py
@@ -3,6 +3,8 @@ import re
 
 from pandas import to_numeric
 
+
+########################################################
 # Get price of item.
 def to_easy_price(price_str):
     now = price_str.split(" ")
@@ -28,9 +30,13 @@ def is_cnt(str):
             return True
     return False
 
+def create_price_format():
+    pre_type = ["\\","*"]
+    return pre_type
+
 def get_price(price_str):
     easy_price = to_easy_price(price_str)
-    price_format = ["\\"]
+    price_format = create_price_format()
     item_name = ""
     cnt = 1
     for now in easy_price:
@@ -50,13 +56,51 @@ def get_price(price_str):
             item_name += now
     return False
 
+########################################################
+# Get price data range
+def is_sum(price_str_list):
+    sum_type = [["合","小","全"],["計","体"]]
+    flags = [False,False]
+    for index, element in enumerate(price_str_list):
+        for flag_index in range(len(flags)):
+            if(flags[flag_index] == True):
+                continue
+            for it in sum_type[flag_index]:
+                if(it in element):
+                    flags[flag_index] = True
+    for it in flags:
+        if(it == False):
+            return False
+    return True
+
+def get_price_range(data):
+    start = 0
+    end = 0
+    price_format = create_price_format()
+    for index, element in enumerate(data):
+        now = to_easy_price(element)
+        if(is_sum(now) == True and end == 0):
+            end = index
+        if(start != 0):
+            continue
+        for now_c in now:
+            for pf in price_format:
+                if(is_price(now_c,pf) == True):
+                    price = re.findall(r'\d+',now_c)
+                    if(len(price) > 0):
+                        start = index
+    return start,end
+
+
 test_dir_path = u"../Beginners/Beginers/result"
-start = 0
-end = 15
+
 
 for it in Path(test_dir_path).glob("*"):
+    print("\n{}".format(it))
     with open(it,"r") as f:
         reader = f.read().split("\n")
+        start,end = get_price_range(reader)
+        print("{} to {}".format(start,end))
         for i in range(start,end):
             now_result = get_price(reader[i])
             if(now_result != False):
